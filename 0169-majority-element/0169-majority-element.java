4cf6dc32f2cf75ac4d2cf858502fdf0d6a59791d
Ishant27 <68013503+Ishant009@users.noreply.github.com>
2025-01-28 11:39:09 +0530

    Time: 21 ms (8.87%), Space: 49.4 MB (88.8%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,11 +4,7 @@ class Solution {
         int number = Integer.MIN_VALUE;
         Map<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i])+1);
-            }else{
-                map.put(nums[i], 1);
-            }
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
             if(map.get(nums[i])>max){
                 number = nums[i];
                 max = map.get(nums[i]);
