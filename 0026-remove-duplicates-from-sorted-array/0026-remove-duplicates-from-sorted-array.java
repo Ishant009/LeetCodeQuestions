388008225c92ee0d9a0438158d02e1b7b69e250f
Ishant27 <68013503+Ishant009@users.noreply.github.com>
2025-01-28 10:57:19 +0530

    Time: 2 ms (11.71%), Space: 45.1 MB (23.09%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,9 +3,7 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         int k=0;
         for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i])+1);
-            }else{
+            if(!map.containsKey(nums[i])){
                 map.put(nums[i], 1);
                 nums[k++] = nums[i];
             }
