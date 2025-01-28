2776eb04011e604b84140fc320d1eec7a5c25a69
Ishant27 <68013503+Ishant009@users.noreply.github.com>
2025-01-28 11:49:51 +0530

    Time: 1 ms (99.82%), Space: 53.1 MB (48.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,18 +3,22 @@ class Solution {
         int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
         
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
+        int count = 0;
+        int candidate = 0;
         
-        n = n / 2;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > n) {
-                return entry.getKey();
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
             }
         }
         
-        return 0;
+        return candidate;
     
     }
 }
