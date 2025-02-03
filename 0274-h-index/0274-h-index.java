72427689ec9adc1e9cfb44430df6a33e5e5830ff
Ishant27 <68013503+Ishant009@users.noreply.github.com>
2025-02-03 11:42:02 +0530

    Time: 2 ms (69.54%), Space: 41.8 MB (40.91%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -2,11 +2,11 @@ class Solution {
     public int hIndex(int[] citations) {
        Arrays.sort(citations);
        int n = citations.length;
-        for(int i = 0; i < n; i++){
-            if(n-i <= citations[i]){
-                return n-i;
-            }
+       for(int i=0;i<n;i++){
+        if(n-i<=citations[i]){
+            return n-i;
         }
-        return 0;
+       }
+       return 0;
     }
 }
