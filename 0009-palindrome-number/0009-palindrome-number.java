ff9a219be5c6103aebeb21e72b34176c93775d1f
Ishant27 <68013503+Ishant009@users.noreply.github.com>
2025-02-13 11:30:59 +0530

    Time: 5 ms (72.63%), Space: 43.9 MB (87.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -5,7 +5,6 @@ class Solution {
         while(x>0){
             res = res*10+(x%10);
             x=x/10;
-            // if(res>Integer.MAX_VALUE || res<Integer.MIN_VALUE) return 0;
         }
         return res==org;
     }
