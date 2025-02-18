d04e65c566390fad3dfc845f53cfb50179529f39
Ishant27 <68013503+Ishant009@users.noreply.github.com>
2025-02-18 10:12:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int n = s.length();
+        if(n!=t.length()) return false;
+        String sl = s.toLowerCase();
+        String tl = t.toLowerCase();
+        int[] arr = new int[200];
+        int[] arr2 = new int[200];
+        for(int i=0;i<n;i++){
+            if(arr[sl.charAt(i)]!=arr2[tl.charAt(i)]) return false;
+            arr[sl.charAt(i)]=i+1;
+            arr2[tl.charAt(i)]=i+1;
+        }
+        // Arrays.sort(arr);
+        // Arrays.sort(arr2);
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]!=arr2[i]) return false;
+        // }
+        return true;
+    }
+}
