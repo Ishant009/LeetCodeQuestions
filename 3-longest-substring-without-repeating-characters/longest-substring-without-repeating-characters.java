961dc114a88bf7119a783b5588ee01af16a8f654
Ishant27 <68013503+Ishant009@users.noreply.github.com>
2026-07-22 00:07:15 +0530

    Time: 8 ms (18.61%) | Memory: 46 MB (60.79%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,23 +3,28 @@ class Solution {
         int maxCount=Integer.MIN_VALUE;
         int count=0;
         int l=0;
-        int h=0;
         int i=0;
-        if(s.length()<=1) return s.length();
-        while(l<s.length()){
-            Set<Character> set = new HashSet<>();
-             while(h<s.length() && !set.contains(s.charAt(h)) ){
-                    set.add(s.charAt(h));
-                    count++;
-                    h++; 
+        int res = Integer.MIN_VALUE;
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(int h=0; h<s.length(); h++){
+            map.put(s.charAt(h), map.getOrDefault(s.charAt(h),0)+1);
+            int k = h-l+1;
+            if(k== map.size()){
+                res = Math.max(res, k);
             }
-            maxCount = Math.max(count, maxCount);
-                h=l;
-                count=0;
-                set.clear();   
-            l++;
-        
+            // System.out.println("before :"+map);
+            while(k>map.size()){
+                // System.out.println(map);
+                map.put(s.charAt(l), map.get(s.charAt(l))-1);
+                if(map.get(s.charAt(l))==0){
+                    map.remove(s.charAt(l));
+                }
+                l++;
+                k = h-l+1;
+            }
+            // System.out.println("after: "+map);
         }
-        return maxCount==Integer.MIN_VALUE? 0: maxCount;
+        return res==Integer.MIN_VALUE?0:res;
     }
 }
