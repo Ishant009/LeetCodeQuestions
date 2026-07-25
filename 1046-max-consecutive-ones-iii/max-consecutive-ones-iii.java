fce6e8f5925edae5008fdef77fd9488c36e3cbc4
Ishant27 <68013503+Ishant009@users.noreply.github.com>
2026-07-26 00:13:32 +0530

    Time: 7 ms (8.41%) | Memory: 52.3 MB (17.94%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -7,8 +7,6 @@ class Solution {
         int maxlen =0;
         for(h=0;h<nums.length;h++){
             arr[nums[h]]++;
-            
-            // int diff = findDifference(arr, k);
             while(arr[0]>k){
                 arr[nums[l]]--;
                 l++;
@@ -16,7 +14,6 @@ class Solution {
             if(arr[0]==k || arr[0]<k){
                 maxlen = Math.max(maxlen, arr[1]+arr[0]);
             }
-            //  System.out.println(Arrays.toString(arr));
         }
        
         return maxlen;
