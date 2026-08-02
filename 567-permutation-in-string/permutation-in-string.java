4d78cd606494ab5823d3f5aba62ef8bd9b3eb098
Ishant27 <68013503+Ishant009@users.noreply.github.com>
2026-08-02 15:49:51 +0530

    Time: 6 ms (87.99%) | Memory: 43.7 MB (93.28%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        
+        if(s2.length()< s1.length()) return false;
+        int h=s1.length();
+        int k= s1.length();
+        int[] arr = new int[26];
+        for(int i=0;i<s1.length();i++){
+            arr[s1.charAt(i)-'a']++;
+        }
+        for(h=0; h<k;h++){
+            arr[s2.charAt(h)-'a']--;
+        }
+        if(checkPermuation(arr)) return true;
+        int count =0;
+        int l=0;
+        while(h<s2.length()){
+            arr[s2.charAt(h)-'a']--;
+            arr[s2.charAt(l)-'a']++;
+            boolean check = checkPermuation(arr);
+            if(check == true) {
+                return true;
+            }
+            l++;
+            h++;
+        }
+        return false;
+
+    }
+
+    public boolean checkPermuation(int[] arr){
+        for(int i=0; i<arr.length;i++){
+            if(arr[i]!=0) return false;
+        }
+        return true;
+    }
+}
