9fe68e282c554125be1df2e6b15399914a25d16e
Ishant27 <68013503+Ishant009@users.noreply.github.com>
2026-08-07 22:55:34 +0530

    Time: 1 ms (77.27%), Space: 46.4 MB (92.6%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -4,13 +4,11 @@ class Solution {
         int a = nums[0];
         int l = 0;
         for(int i=1;i<nums.length;i++){
-            System.out.println(nums[l] +"    "+nums[i]);
             if(nums[l]==nums[i]){
                 continue;
             }
             nums[++l]=nums[i];
         }
-        System.out.println(Arrays.toString(nums));
         return l+1;
         
     }
